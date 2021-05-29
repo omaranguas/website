a06191c8cde7223d1f763520c1d596fe3640a4d1
Omar Anguas <fcomaranguas@gmail.com>
2021-05-28 22:30:18 -0500

    Changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,13 +5,12 @@ import menuToggle from "./toggle_menu.js";
 
 const d = document;
 
-window.addEventListener('load', (e) => {
-  const $loader = d.getElementById('loading');
+window.addEventListener("load", (e) => {
+  const $loader = d.getElementById("loading");
   setTimeout(() => {
     d.body.removeChild($loader);
-    d.getElementById('content').classList.remove('hidden');
-  }, 3000);
-
+    d.getElementById("content").classList.remove("hidden");
+  }, 1000);
 });
 
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -19,28 +18,6 @@ d.addEventListener("DOMContentLoaded", (e) => {
   activeLink();
   hideScrollButtonAndHeader("scroll-top");
   validateForm();
-
-  /* const scrollReveal = ScrollReveal({
-    origin: "top",
-    distance: "100px",
-    duration: 1000,
-    reset: true,
-  });
-
-  scrollReveal.reveal(
-    `.section__title, .section__subtitle,
-      .home__data, .home__img, .home__social,
-      .about__data, .about__img, 
-      .skills__header, .skills__content,
-      .portfolio__img,
-      .education__content,
-      .contact__box, .contact__input, .button,
-      .footer__container, .footer__social
-      `,
-    {
-      interval: 200,
-    }
-  ); */
 });
 
 changeTheme();
